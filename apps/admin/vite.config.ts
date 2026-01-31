0e489a6f977e829297a414467579b20132fbaea3
ann <3786329149@qq.com>
2026-01-31 14:12:23 +0800

    feat: 暂停开发

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@repo/shadcn-ui": path.resolve(
+        __dirname,
+        "../../packages/shadcn-ui/src",
+      ),
+    },
+  },
 })
---
+++ packages/shadcn-ui/eslint.config.mjs
@@ -0,0 +1,9 @@
+import { config as reactConfig } from "@repo/eslint-config/react-internal";
+
+/** @type {import("eslint").Linter.Config} */
+export default [
+  ...reactConfig,
+  {
+    ignores: ["dist/**"],
+  },
+];
